01f4497a8c6b78f3ca9a94eb554f8e5d96a5ab28
sprakash277 <sprakash277>
2022-07-17 17:08:52 +0000

    Workshop Changes

--- Lab/Instructor/02-Delta-Live-Table/02.0-Setup_Variables.py
+++ Lab/Instructor/02-Delta-Live-Table/02.0-Setup_Variables.py
@@ -17,3 +17,90 @@ cloud_storage_path = f"/Users/{current_user}/databricks_workshop/{db_prefix}"
 dlt_target_path = cloud_storage_path + "/" + "dlt"
 
 print("Database Name :-  {}".format(dbName))
+
+# COMMAND ----------
+
+# DBTITLE 1,0 - DLT CLUSTER POLICY JSON
+{
+  "cluster_type": {
+    "type": "fixed",
+    "value": "dlt"
+  }
+}
+
+# COMMAND ----------
+
+# DBTITLE 1,1 - DLT JOB JSON
+{
+    "clusters": [
+        {
+            "label": "default",
+            "policy_id": "60628DC03F000865",
+            "autoscale": {
+                "min_workers": 1,
+                "max_workers": 5
+            }
+        },
+        {
+            "label": "maintenance",
+            "policy_id": "60628DC03F000865"
+        }
+    ],
+    "development": true,
+    "continuous": false,
+    "edition": "advanced",
+    "photon": false,
+    "libraries": [
+        {
+            "notebook": {
+                "path": "/Repos/DLT_PIPELINE_RUN/WorkShop/Lab/Instructor/02-Delta-Live-Table/02.1-SQL-Delta-Live-Table-Ingestion"
+            }
+        }
+    ],
+    "name": "DLT_PIPELINE",
+    "storage": "/workshop/dlt_pipeline",
+    "configuration": {
+        "userID": "FILL_IN_USER_ID"
+    },
+    "target": "DLT_EXERCISE"
+}
+
+
+# COMMAND ----------
+
+# DBTITLE 1,2 - DLT CDC JOB JSON
+{
+       "clusters": [
+        {
+            "label": "default",
+            "policy_id": "60628DC03F000865",
+            "autoscale": {
+                "min_workers": 1,
+                "max_workers": 5
+            }
+        },
+        {
+            "label": "maintenance",
+            "policy_id": "60628DC03F000865"
+        }
+    ],
+    "development": true,
+    "continuous": false,
+    "edition": "advanced",
+    "photon": false,
+    "libraries": [
+        {
+            "notebook": {
+"path":"/Repos/DLT_PIPELINE_RUN/WorkShop/Lab/Instructor/02-Delta-Live-Table/CDC/2-Retail_DLT_CDC_sql"
+            }
+        }
+    ],
+    "name": "DLT_CDC",
+    "configuration": {
+        "userID": "FILL_IN_YOUR_ID",
+        "source": "/tmp/demo/cdc_raw",
+        "pipelines.applyChangesPreviewEnabled": "true"
+    },
+    "target": "DLT_EXERCISE"
+}
+
